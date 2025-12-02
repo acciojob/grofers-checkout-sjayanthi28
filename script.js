0e5cf59976037983eb1e35acc7f8208275c81b8c
sjayanthi28 <sjayanthisjayanthi88@gmail.com>
2025-12-02 14:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
-};
-
+	const prices=document.querySelectorAll('.prices');
+	let total=0;
+	prices.forEach((p => {
+		total+=parsefloat(p.textContent)
+	}));
+	cosnt table=document.querySelector('table');
+	const newRow=document.createElement("tr");
+	const labelcell=document.createElement("td");
+	const tablecell=document.createElement("td");
+	totalcell.textContent='total';
+	labelcell.textContent='totalprice';
+	new id='ans';
+	newRow.append(labelcell,tablecell);
+	table.appendchild('newRow')
 getSumBtn.addEventListener("click", getSum);
 
